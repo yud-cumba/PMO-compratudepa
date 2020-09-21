aac0b0045a8a7ec5b55f47c302910608b91b4ff2
Yudith <yudith@MacBook-Air-de-administrador.local>
2020-09-21 15:25:52 -0500

    boilerplate vue

---
+++ src/components/Maps.vue
@@ -0,0 +1,28 @@
+<template>
+    <div>
+
+    </div>
+</template>
+
+<script>
+    var ee = require('@google/earthengine');
+
+    // Authenticate using one (but not both) of the methods below.
+    // ee.data.authenticateViaOauth(YOUR_CLIENT_ID);
+    // ee.data.authenticateViaPrivateKey(YOUR_PRIVATE_KEY);
+
+    ee.initialize();
+
+    // Run an Earth Engine script.
+    var image = new ee.Image('srtm90_v4');
+    image.getMap({min: 0, max: 1000}, function(map) {
+    console.log(map);
+    });
+    export default {
+        
+    }
+</script>
+
+<style lang="scss" scoped>
+
+</style>
--- src/main.js
+++ src/main.js
@@ -1,8 +1,19 @@
 import Vue from 'vue'
 import App from './App.vue'
-
+import VueRouter from 'vue-router'
+import vuetify from './plugins/vuetify';
+import Home from './views/Home'
+Vue.use(VueRouter)
 Vue.config.productionTip = false
 
+const router = new VueRouter({
+  mode: 'history',
+  routes: [
+  { path: '/', component: Home },
+  ]
+})
 new Vue({
+  router,
+  vuetify,
   render: h => h(App),
 }).$mount('#app')
