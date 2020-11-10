0f9679f4a0d6f71ff240863074ad3787bc8726b4
Yudith <yudith@MacBook-Air-de-Yud.local>
2020-11-09 22:59:55 -0500

    feat(PrivacyPoliy): Implementacion terminos y condiciones

--- src/components/PrivacyPolicy.vue
+++ src/components/PrivacyPolicy.vue
@@ -2,7 +2,7 @@
   <div>
     <v-dialog
       v-model="dialog"
-      width="500"
+      width="700"
     >
       <template v-slot:activator="{ on, attrs }">
         <p class="pt-1 blue--text text-decoration-underline"
@@ -13,21 +13,57 @@
         </p>
       </template>
 
-      <v-card>
-        <v-card-title class="headline grey lighten-2">
-          Privacy Policy
+      <v-card class="pa-3 rounded-xl">
+        <v-card-title class="headline mb-7 lighten-2" color="green">
+         TÉRMINOS Y CONDICIONES
         </v-card-title>
-
+         <v-card-subtitle color="green">
+        Términos de uso
+        </v-card-subtitle>
         <v-card-text>
-          Lorem ipsum dolor sit amet,
+          Los presentes Términos y Condiciones rigen la relación entre usted
+          (en adelante referido como “Usuario”) y Mi Doc Technologies S.A.P.I. de C.V.
+          (en adelante referido como “MiDoc”) con relación a los servicios y productos
+          disponibles al Usuario a través del
+          sitio web www.midocapp.com (en adelante
+          referido como “Página Web”)
+          y la app móvil (en adelante referido como “Aplicación Móvil”) ;
+          a su vez, los presentes términos y condiciones
+          regulan el uso de la Página Web, del que MiDoc, con razón social MiDoc
+          Technologies S.A.P.I. de C.V,
+          es propietario, la cual es una Sociedad Anónima Promotora de Inversión,
+          con domicilio fiscal en Puebla 403, Col. Roma Norte, Delegación Cuauhtémoc,
+           Ciudad de México, C.P. 06700.
         </v-card-text>
-
-        <v-divider></v-divider>
-
-        <v-card-actions>
-          <v-spacer></v-spacer>
+        <v-card-subtitle color="green">
+        Aceptación de los términos y condiciones
+        </v-card-subtitle>
+         <v-card-text>
+          Los presentes Términos y Condiciones rigen la relación entre usted
+          (en adelante referido como “Usuario”) y Mi Doc Technologies S.A.P.I. de C.V.
+          (en adelante referido como “MiDoc”) con relación a los servicios y productos
+          disponibles al Usuario a través del
+          sitio web www.midocapp.com (en adelante
+          referido como “Página Web”)
+          y la app móvil (en adelante referido como “Aplicación Móvil”) ;
+          a su vez, los presentes términos y condiciones
+          regulan el uso de la Página Web, del que MiDoc, con razón social MiDoc
+          Technologies S.A.P.I. de C.V,
+          es propietario, la cual es una Sociedad Anónima Promotora de Inversión,
+          con domicilio fiscal en Puebla 403, Col. Roma Norte, Delegación Cuauhtémoc,
+           Ciudad de México, C.P. 06700.
+        </v-card-text>
+        <v-card-actions class="d-flex justify-center">
           <v-btn
-            color="primary"
+            color="green"
+            outlined
+            text
+            @click="noAcept"
+          >
+            No acepto
+          </v-btn>
+          <v-btn
+            class="green"
             text
             @click="closeModal"
           >
@@ -48,6 +84,10 @@ export default {
     };
   },
   methods: {
+    noAcept() {
+      this.dialog = false;
+      this.acceptConditions(false);
+    },
     closeModal() {
       this.dialog = false;
       this.acceptConditions(true);
