8c6c62157bdc8b023d4f610b522389cadc4b6cb1
Yudith <yudith@MacBook-Air-de-Yud.local>
2020-11-29 09:06:43 -0500

    feat: Implementacion de inicial tabs de creditos

---
+++ src/components/Credit.vue
@@ -0,0 +1,119 @@
+<template>
+    <div class="d-flex justify-center align-center">
+        <v-card class="ma-12">
+            <v-stepper
+            class="px-7"
+            v-model="e6"
+            vertical
+        >
+            <v-stepper-step
+            :complete="e6 > 1"
+            step="1"
+            >
+            Select an app
+            <small>Summarize if needed</small>
+            </v-stepper-step>
+
+            <v-stepper-content step="1">
+            <v-card
+                color="grey lighten-1"
+                class="mb-12"
+                height="200px"
+            ></v-card>
+            <v-btn
+                color="primary"
+                @click="e6 = 2"
+            >
+                Continue
+            </v-btn>
+            <v-btn text>
+                Cancel
+            </v-btn>
+            </v-stepper-content>
+
+            <v-stepper-step
+            :complete="e6 > 2"
+            step="2"
+            >
+            Configure analytics for this app
+            </v-stepper-step>
+
+            <v-stepper-content step="2">
+            <v-card
+                color="grey lighten-1"
+                class="mb-12"
+                height="200px"
+            ></v-card>
+            <v-btn
+                color="primary"
+                @click="e6 = 3"
+            >
+                Continue
+            </v-btn>
+            <v-btn text>
+                Cancel
+            </v-btn>
+            </v-stepper-content>
+
+            <v-stepper-step
+            :complete="e6 > 3"
+            step="3"
+            >
+            Select an ad format and name ad unit
+            </v-stepper-step>
+
+            <v-stepper-content step="3">
+            <v-card
+                color="grey lighten-1"
+                class="mb-12"
+                height="200px"
+            ></v-card>
+            <v-btn
+                color="primary"
+                @click="e6 = 4"
+            >
+                Continue
+            </v-btn>
+            <v-btn text>
+                Cancel
+            </v-btn>
+            </v-stepper-content>
+
+            <v-stepper-step step="4">
+            View setup instructions
+            </v-stepper-step>
+            <v-stepper-content step="4">
+            <v-card
+                color="grey lighten-1"
+                class="mb-12"
+                height="200px"
+            ></v-card>
+            <v-btn
+                color="primary"
+                @click="e6 = 1"
+            >
+                Continue
+            </v-btn>
+            <v-btn text>
+                Cancel
+            </v-btn>
+            </v-stepper-content>
+            </v-stepper>
+        </v-card>
+        <div class="ma-12">
+            <h2 class="ma-12">Te acompañamos en el proceso </h2>
+            <v-img src="../assets/co-workers.svg">
+            </v-img>
+            </div>
+    </div>
+</template>
+
+<script>
+export default {
+
+};
+</script>
+
+<style lang="scss" scoped>
+
+</style>
--- src/views/Advice.vue
+++ src/views/Advice.vue
@@ -7,20 +7,54 @@
       <v-btn @click="logout">
         Cerrar sesión
       </v-btn>
+      <h1>TIPS</h1>
+      <v-tabs
+      v-model="tab"
+      background-color="white"
+      color="green"
+      grow
+    >
+      <v-tab
+        v-for="item in items"
+        :key="item"
+      >
+        {{ item }}
+      </v-tab>
+    </v-tabs>
+
+    <v-tabs-items v-model="tab">
+      <v-tab-item>
+        <Credit/>
+      </v-tab-item>
+      <v-tab-item>
+      </v-tab-item>
+      <v-tab-item>
+      </v-tab-item>
+      <v-tab-item>
+      </v-tab-item>
+    </v-tabs-items>
     </div>
   </div>
 </template>
 
 <script>
 import SignUp from '../components/SignUp.vue';
+import Credit from '../components/Credit.vue';
+
 import { verifyIsLogin, logout } from '../firebase/auth';
 
 export default {
   components: {
+    Credit,
     SignUp,
   },
   data: () => ({
     isLogin: false,
+    tab: null,
+    items: [
+      '1 CRÉDITO HIPOTECARIO', '2 BUENAS PRÁCTICAS MINUTA', '3 CHECKLIST DOCUMENTACIÓN',
+    ],
+    text: 'Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat.',
   }),
   methods: {
     logout,
