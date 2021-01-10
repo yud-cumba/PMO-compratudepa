01a4dba26d364a8021090f81f870fbe2c50c3790
yud-cumba <yudith@MacBook-Air-de-Yud.local>
2021-01-09 23:47:36 -0500

    -

--- src/firebase/storage.js
+++
@@ -1,5 +0,0 @@
-import firebase from '../keys/firebaseConfig';
-
-export const addFileToStorage = (refPath, files) => files.map((file) => firebase.storage().ref(`${refPath}/${file.name}`).put(file));
-
-export const getFileFromStorage = (path) => firebase.storage().ref().child(path).getDownloadURL();
